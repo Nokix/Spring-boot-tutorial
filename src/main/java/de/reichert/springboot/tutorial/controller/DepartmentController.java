f1d886881888702e41b0edb3de9aaf626b894413
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-01-02 21:20:16 +0100

    Department Entity

---
+++ src/main/java/de/reichert/springboot/tutorial/controller/DepartmentController.java
@@ -0,0 +1,8 @@
+package de.reichert.springboot.tutorial.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DepartmentController {
+
+}
---
+++ src/main/java/de/reichert/springboot/tutorial/entity/Department.java
@@ -0,0 +1,61 @@
+package de.reichert.springboot.tutorial.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "dep")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "dep_name", nullable = false)
+    private String name;
+    private String address;
+    //@Transient
+    private String code;
+
+    public Department() {
+    }
+
+    // Konstruktoren, Getter, Setter,...
+
+    public Department(String name, String address, String code) {
+        this.name = name;
+        this.address = address;
+        this.code = code;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
