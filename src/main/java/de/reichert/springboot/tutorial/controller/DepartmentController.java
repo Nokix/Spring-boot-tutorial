aee92c2ef6ec8667ff719225972c326a6b64d732
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-01-03 11:55:59 +0100

    GetMapping getAllDepartments

--- src/main/java/de/reichert/springboot/tutorial/controller/DepartmentController.java
+++ src/main/java/de/reichert/springboot/tutorial/controller/DepartmentController.java
@@ -3,14 +3,22 @@ package de.reichert.springboot.tutorial.controller;
 import de.reichert.springboot.tutorial.entity.Department;
 import de.reichert.springboot.tutorial.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
+
+    @GetMapping("/department")
+    public List<Department> getAllDepartments() {
+        return departmentService.getAllDepartments();
+    }
 
     @PostMapping("/department")
     public Department saveDepartment(@RequestBody Department department) {
--- src/main/java/de/reichert/springboot/tutorial/service/DepartmentService.java
+++ src/main/java/de/reichert/springboot/tutorial/service/DepartmentService.java
@@ -2,6 +2,10 @@ package de.reichert.springboot.tutorial.service;
 
 import de.reichert.springboot.tutorial.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     Department saveDepartment(Department department);
+
+    List<Department> getAllDepartments();
 }
