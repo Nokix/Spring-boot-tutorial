00a577fbc78a0581028959476fef826a3e50d259
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-01-04 11:05:28 +0100

    Exception Tests

--- src/test/java/de/reichert/springboot/tutorial/service/DepartmentServiceTest.java
+++ src/test/java/de/reichert/springboot/tutorial/service/DepartmentServiceTest.java
@@ -1,6 +1,7 @@
 package de.reichert.springboot.tutorial.service;
 
 import de.reichert.springboot.tutorial.entity.Department;
+import de.reichert.springboot.tutorial.error.DepartmentNotFoundException;
 import de.reichert.springboot.tutorial.repository.DepartmentRepository;
 import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
@@ -28,6 +29,12 @@ class DepartmentServiceTest {
 
         Mockito.when(departmentRepository.findByName("IT"))
                 .thenReturn(Optional.of(department));
+
+        Mockito.when(departmentRepository.findById(1L))
+                .thenReturn(Optional.of(department));
+
+        Mockito.when(departmentRepository.findById(2L))
+                .thenReturn(Optional.empty());
     }
 
     @Test
@@ -36,6 +43,21 @@ class DepartmentServiceTest {
         String departmentName = "IT";
         Optional<Department> found = departmentService.fetchDepartmentByName(departmentName);
         assertEquals(departmentName, found.get().getName());
+    }
+
+    @Test
+    @DisplayName("Get Data based on valid Id")
+    void fetchDepartmentById_valid() throws DepartmentNotFoundException {
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentById(1L);
+        assertEquals(departmentName, found.getName());
+    }
+
+    @Test
+    @DisplayName("Throw DepartmentNotFoundException on invalid Id")
+    void fetchDepartmentById_invalid() {
+        assertThrows(DepartmentNotFoundException.class,
+                () -> departmentService.fetchDepartmentById(2L));
     }
 }
 
